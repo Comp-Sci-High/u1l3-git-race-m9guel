11d8b83705c95c67cc9a94e91c89f1a86b70ba60
m9guel <miguel.sanchez26@compscihigh.org>
2024-09-18 15:20:36 +0000

    printed go to deli order

--- more.js
+++ more.js
@@ -4,6 +4,6 @@ console.log("Popeyes")
 // Stage and commit this change with an appropriate message
 // Task 5
 // Print your go to deli order
-
+console.log("Bacon, egg, and cheese")
 // Stage and commit this change with an appropriate message
 // Go to README.md
